a433f6a230369d61dcd0eaf32c698acc0ba113d4
Pranavkm-10 <164546071+Pranavkm-10@users.noreply.github.com>
2025-02-11 16:39:09 +0530

    Create Login.js

---
+++ College Event Frontend/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to College Event Management</h1>
+      <p>Discover and participate in college events.</p>
+    </div>
+  );
+};
+
+export default Home;
