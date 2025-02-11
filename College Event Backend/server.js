0ccae0c21bd0cb870631430fa498aae843fa0780
Pranavkm-10 <164546071+Pranavkm-10@users.noreply.github.com>
2025-02-11 20:28:25 +0530

    Update server.js

--- College Event Backend/server.js
+++ College Event Backend/server.js
@@ -1,20 +1,17 @@
-/// server.js
 const express = require("express");
-const cors = require("cors");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
-const eventRoutes = require("./routes/eventRoutes");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/events", eventRoutes);
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/events", require("./routes/eventRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
