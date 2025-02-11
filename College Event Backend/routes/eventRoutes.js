c6056381b82ac08c6c3e615bdc3a805d30c3f9d1
Pranavkm-10 <164546071+Pranavkm-10@users.noreply.github.com>
2025-02-11 17:30:11 +0530

    Update eventRoutes.js

--- College Event Backend/routes/eventRoutes.js
+++ College Event Backend/routes/eventRoutes.js
@@ -1,1 +1,7 @@
-
+const express = require("express");
+const { createEvent, getEvents } = require("../controllers/eventController");
+const authMiddleware = require("../middleware/authMiddleware");
+const router = express.Router();
+router.post("/create", authMiddleware, createEvent);
+router.get("/all", getEvents);
+module.exports = router;
