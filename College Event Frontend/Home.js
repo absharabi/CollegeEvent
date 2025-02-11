81e98b134c1f71948d64aa6600f2a89359efd2e1
Pranavkm-10 <164546071+Pranavkm-10@users.noreply.github.com>
2025-02-11 16:37:06 +0530

    Create Home.js

---
+++ College Event Frontend/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to College Event Management</h1>
+      <p>Discover and participate in college events.</p>
+    </div>
+  );
+};
