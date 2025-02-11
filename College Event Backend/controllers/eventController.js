52485faafaaa6355dafbdf9f2cb8939d804b166c
Pranavkm-10 <164546071+Pranavkm-10@users.noreply.github.com>
2025-02-11 17:31:12 +0530

    Update eventController.js

--- College Event Backend/controllers/eventController.js
+++ College Event Backend/controllers/eventController.js
@@ -1,1 +1,18 @@
-
+const Event = require("../models/Event");
+exports.createEvent = async (req, res) => {
+  try {
+    const event = new Event({ ...req.body, createdBy: req.user.id });
+    await event.save();
+    res.status(201).json(event);
+  } catch (error) {
+    res.status(500).json({ error: "Event creation failed" });
+  }
+};
+exports.getEvents = async (req, res) => {
+  try {
+    const events = await Event.find().populate("createdBy", "name");
+    res.json(events);
+  } catch (error) {
+    res.status(500).json({ error: "Fetching events failed" });
+  }
+};
