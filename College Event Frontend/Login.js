1d8ec6dcec29d62677287f77cd1eda342e45fefe
Pranavkm-10 <164546071+Pranavkm-10@users.noreply.github.com>
2025-02-11 16:40:06 +0530

    Update Login.js

--- College Event Frontend/Login.js
+++ College Event Frontend/Login.js
@@ -10,3 +10,17 @@ const Home = () => {
 };
 
 export default Home;
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
