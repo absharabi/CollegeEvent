2485719ec5e0216824c3476bdc29d038bd4f0fac
Pranavkm-10 <164546071+Pranavkm-10@users.noreply.github.com>
2025-02-11 20:24:09 +0530

    Update Event.js

--- College Event Backend/models/Event.js
+++ College Event Backend/models/Event.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
-const EventSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  date: Date,
-  location: String,
-  category: String,
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
+const eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true },
+  location: { type: String, required: true },
+  category: { type: String, enum: ["Sports", "Tech", "Cultural"], required: true },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
-module.exports = mongoose.model("Event", EventSchema);
+
+module.exports = mongoose.model("Event", eventSchema);
