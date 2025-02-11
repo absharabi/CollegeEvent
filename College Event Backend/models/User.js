1be902f4fa88e6edfa6f892e4cf999a20288505c
Pranavkm-10 <164546071+Pranavkm-10@users.noreply.github.com>
2025-02-11 17:28:48 +0530

    Update User.js

--- College Event Backend/models/User.js
+++ College Event Backend/models/User.js
@@ -1,1 +1,8 @@
-
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ["student", "organizer", "admin"], default: "student" },
+});
+module.exports = mongoose.model("User", UserSchema);
