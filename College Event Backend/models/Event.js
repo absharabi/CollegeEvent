578914481652fb5b5a4f64223d43cb2c5319bbf4
Pranavkm-10 <164546071+Pranavkm-10@users.noreply.github.com>
2025-02-11 17:29:19 +0530

    Update Event.js

--- College Event Backend/models/Event.js
+++ College Event Backend/models/Event.js
@@ -1,1 +1,10 @@
-
+const mongoose = require("mongoose");
+const EventSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  date: Date,
+  location: String,
+  category: String,
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+module.exports = mongoose.model("Event", EventSchema);
